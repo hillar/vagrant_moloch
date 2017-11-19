39e024daa0c2e252c1ef657c580d8ef97c6f623e
Hillar Aarelaid <hillar@aarelaid.net>
2017-11-19 02:36:33 +0200

    make influxdb proto in bash
    so javascript is doing only stats

--- moloUniq2influx.bash
+++ moloUniq2influx.bash
@@ -1,27 +1,85 @@
+#!/bin/bash
 
+# get unique.txt for fields
+# calculate stats on count vector
+# send result to influxdb
+
+DEBUG="0"
+MOLO="192.168.10.11:8005"
+INFLUX="192.168.10.12:8086"
+#url encoded filter
+#EXPRESSION="expression=databytes.src%3E0%26%26databytes.dst%3E0"
+EXPRESSION=""
+FIELDS="a2 a1 p1 p2 pa sl db dns.status-term tlsja3-term fb1 fb2"
+# 'timings' in seconds
+WINDOW="120"
+OVERLAP="60"
+WAITFORELASTIC="90"
+MOLOTIMEOUT="2"
+
+# startTime & stopTime are from now()
+STOP=$WAITFORELASTIC
+START=$((WINDOW+STOP))
+# time for one roundtrip
+INTERVAL=$(((WINDOW-OVERLAP)))
+
+echo "$(date) INFO: starting with window $WINDOW overlap $OVERLAP elawait $WAITFORELASTIC molotimeout $MOLOTIMEOUT fields $FIELDS expression $EXPRESSION "
 while true
 do
  vlan="dummy"
  #for vlan in 89 82 81 73 83 88 85 100 71
  #do
-  for field in a2 a1 p1 p2 pa sl db
-  do
-  echo "$vlan $field"
-  end=$(date --date='2 minutes ago' +%s)
-  start=$(date --date='12 minutes ago' +%s)
+  timer_start=$(date +%s)
+  end=$(date --date="$STOP seconds ago" +%s)
+  start=$(date --date="$START seconds ago" +%s)
   time=$(echo "startTime=$start&stopTime=$end")
-  #URL="http://localhost:8005/unique.txt?$time&expression=databytes.src%3E0%26%26databytes.dst%3E0%26%26vlan%3D$vlan&field=$field&counts=1"
-URL="http://192.168.10.11:8005/unique.txt?$time&expression=databytes.src%3E0%26%26databytes.dst%3E0&field=$field&counts=1"
-  echo $URL
-  curl -m 2 -s --digest -uadmin:admin $URL > $vlan.$field.txt
-  # send beartbeat
-  curl -i -XPOST 'http://192.168.10.12:8086/write?db=molouniqs' --data-binary "heartbeat,field=$field,vlan=$vlan value=1 $(date +%s%N)"
-  [ $? -eq 0 ] || break
-  vl=$(wc -l $vlan.$field.txt | cut -f1 -d" ")
-  #[ $vl -eq 0 ] || break
-  cut -f2 -d, $vlan.$field.txt | node moloUniq2influx.js $vlan $field
-  sleep 1
+  data=""
+  nodata=""
+  for field in $FIELDS
+  do
+    #URL="http://localhost:8005/unique.txt?$time&expression=databytes.src%3E0%26%26databytes.dst%3E0%26%26vlan%3D$vlan&field=$field&counts=1"
+    URL="http://$MOLO/unique.txt?$time&$EXPRESSION&field=$field&counts=1"
+    [[ $DEBUG -gt 0 ]] && echo $URL
+    curl -m $MOLOTIMEOUT -s --digest -uadmin:admin $URL > $vlan.$field.txt
+    molook=$?
+    [[ $molook -eq 0 ]] || echo "$(date) ERROR: moloch ($(MOLO)) did not respond in $MOLOTIMEOUT"
+    [[ $molook -eq 0 ]] || break
+    NOW=$(date +%s)
+    # send beartbeat
+    mololines=$(wc -l $vlan.$field.txt | cut -f1 -d" ")
+    # see https://docs.influxdata.com/influxdb/v1.3/guides/writing_data/#writing-data-using-the-http-api
+    curl -s -XPOST "http://$INFLUX/write?db=molouniqs&precision=s" --data-binary "heartbeat,field=$field,vlan=$vlan value=$mololines $NOW"
+    # do somestats ...
+    observationtime=$(date --date="$START seconds ago" +%s)
+    rm $vlan.$field.stats
+    touch $vlan.$field.stats
+    [[ $mololines -gt 0 ]] && data="$data$field;"
+    [[ $mololines -gt 0 ]] || nodata="$nodata$field;"
+    # calculate single number values of vector
+    # see https://github.com/hillar/atoll.js
+    #                         take counts only
+    [[ $mololines -gt 0 ]] && cut -f2 -d, $vlan.$field.txt | node vectorstats.js > $vlan.$field.stats
+    statslines=$(wc -l $vlan.$field.stats | cut -f1 -d" ")
+    [[ $DEBUG -gt 0 ]] && echo  "$vlan $field $mololines $statslines"
+    #[[ $statslines -eq 0 ]] ||
+    cat $vlan.$field.stats | while read line; do
+      # see https://docs.influxdata.com/influxdb/v1.3/guides/writing_data/#writing-points-from-a-file
+      echo $line | awk '{print $1 ",field='$field' value=" $2 " '$observationtime'"}';
+      #sf=$(echo $line | cut -f1 -d" ")
+      #sv=$(echo $line | cut -f2 -d" ")
+      #sd="$sf,field=$field,vlan=$vlan value=$sv $observationtime"
+      #echo $sd
+      #curl -s -XPOST "http://$INFLUX/write?db=molouniqs&precision=s" --data-binary "$sd"
+    done | curl -s -m $MOLOTIMEOUT -XPOST "http://$INFLUX/write?db=molouniqs&precision=s" --data-binary @-
+    influxok=$?
+    [[ $influxok -eq 0 ]] || echo "$(date) ERROR: Influxdb ($INFLUX) did not respond in $MOLOTIMEOUT"
+    sleep 1
   done
-  sleep 30
+  timer_end=$(date +%s)
+  timespent=$((timer_end-timer_start))
+  echo "$(date) INFO: took $timespent sleep $((INTERVAL-timespent)) data:$data no data:$nodata "
+  #check timespent is not more than interval
+  [[ $((INTERVAL-timespent)) -gt 0 ]] || echo "$(date) WARNING: quering moloch took ($timespent) longer than one round ($INTERVAL) !"
+  [[ $((INTERVAL-timespent)) -gt 0 ]] && sleep $((INTERVAL-timespent))
  #done
 done
---
+++ vectorstats.js
@@ -0,0 +1,45 @@
+
+// read vector from stdin
+// apply standard stat functions what return single numeric value
+// and write to stdout
+
+var atoll = require('./atoll.js');
+
+// make list of functions, what return single numeric value
+function _atollNumericFunctions() {
+  var ret = {};
+  var obj = atoll([1, 2, 3, 4, 5, 6]);
+  for (var prop in obj) {
+    if (typeof(obj[prop]) === 'function') {
+      try {
+        var tmp = obj[prop]();
+      } catch (err) {
+        //noop
+      } finally {
+        if (typeof(tmp) === 'number') {
+          ret[prop] = NaN;
+        }
+      }
+    }
+  }
+  return ret;
+}
+
+var functions = _atollNumericFunctions();
+
+// reading from stdin
+var fs = require("fs");
+fs.readFile("/dev/stdin", "utf8", function(error, contents) {
+  var a = contents.split("\n");
+  a.pop();
+  if (a.length > 0) {
+    var stat = atoll(a.map(Number), true);
+    var measures = {}
+    for (var f in functions) {
+      var value = stat[f]();
+      if (value != 0 && value != Infinity && value != -Infinity && !isNaN(value)) {
+        console.log(f,value)
+      }
+    }
+  }
+});
